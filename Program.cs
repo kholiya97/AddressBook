8f6fd82b90bafdc0c5411e17ea5d7791f2ad01cc
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-04-19 02:36:32 +0530

    Added file of UC2_Add_NEW_CONTACT_ADD.BOOK

--- Program.cs
+++ Program.cs
@@ -4,14 +4,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AddressBook
-{
-    class Program
+namespace Address_Book_System
+{    /*     Ability to add a new Contact to Address Book - Use Console to add person details from AddressBookMain class
+            - Use Object Oriented Concepts to manage relationship between AddressBook and Contact Person
+    */
+    class Person
     {
-        static void Main(string[] args)
+        private String lname, address, city, state, phone, zip, email; //Declaring (Creating) Variables
+        public Person(String fname, String lname, String address, String city, String state, String phone, String zip, string email)
+        {                                                   //constructor
+            this.FirstName = fname;
+            this.LastName = lname;
+            this.Address = address;
+            this.City = city;
+            this.State = state;
+            this.PhoneNo = phone;
+            this.ZipCode = zip;
+            this.email = email;
+        }
+
+        public string FirstName { get; set; }         // get method returns the value of the variable FirstName.
+                                                      // set method assigns a value to the name variable.
+        public string LastName { get => lname; set => lname = value; }
+        public string Address { get => address; set => address = value; }
+        public string City { get => city; set => city = value; }
+        public string State { get => state; set => state = value; }
+        public string PhoneNo { get => phone; set => phone = value; }
+        public string ZipCode { get => zip; set => zip = value; }
+        public string Email { get => email; set => email = value; }
+        public override string ToString() //Tostring  method store value
         {
-            Console.WriteLine("Welcome to Address Book Program");
+            return "FirstName:- " + FirstName + "\nLastName:- " + LastName + " \nAddress:- " + Address + " \nCity:- " + City
+                 + "\nState:- " + State + "\nZipCode:- " + ZipCode + "\nPhoneNo:- " + PhoneNo + "\nEmail:- " + email + " " + "\n";
+        }
+         static void Main(string[]args)
+        {
+            Console.Write("Enter First Name:- ");  // Take input 
+            string firstName = Console.ReadLine(); //Store input in firstName variable
+            Console.Write("Enter Last Name:- ");
+            string lastName = Console.ReadLine();
+            Console.Write("Enter Address:- ");
+            string address = Console.ReadLine();
+            Console.Write("Enter City:- ");
+            string city = Console.ReadLine();
+            Console.Write("Enter State:- ");
+            string state = Console.ReadLine();
+            Console.Write("Enter Zip Code :- ");
+            string zip = Console.ReadLine();
+            Console.Write("Enter Phone Number:- ");
+            string pNumber = Console.ReadLine();
+            Console.Write("Enter Email:- ");
+            string Email = Console.ReadLine();
+            Person person = new Person(firstName, lastName, address, city, state, pNumber,zip, Email);
+            Person person1 = new Person(firstName, lastName, address, city, state, pNumber, zip, Email);
+            Console.WriteLine(person);
+            Console.ReadLine();
+            Console.WriteLine(person1);
             Console.ReadLine();
         }
     }
+
 }
+
+
