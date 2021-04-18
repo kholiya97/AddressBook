9e2a3c803d9413be87ca9e94b9bc08fbb0e4b3b2
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-04-18 23:29:32 +0530

    Added file of UC1_CREATING_A_CONTACT

--- Program.cs
+++ Program.cs
@@ -1,17 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AddressBook
 {
-    class Program
+    class Contacts
     {
-        static void Main(string[] args)
+        static void Main(string[] args)  //Main method
         {
-            Console.WriteLine("Welcome to Address Book Program");
-            Console.ReadLine();
+            Console.WriteLine("******* Welcome To AddressBook *******");
+
+            Console.Write("Enter First Name:- ");  // Take input 
+            string firstName = Console.ReadLine(); //Store input in firstName variable
+            Console.Write("Enter Last Name:- ");
+            string lastName = Console.ReadLine();
+            Console.Write("Enter Address:- ");
+            string address = Console.ReadLine();
+            Console.Write("Enter City:- ");
+            string city = Console.ReadLine();
+            Console.Write("Enter State:- ");
+            string state = Console.ReadLine();
+            Console.Write("Enter Zip Code :- ");
+            string zip = Console.ReadLine();
+            Console.Write("Enter Phone Number:- ");
+            string pNumber = Console.ReadLine();
+            Console.Write("Enter Email:- ");
+            string Email = Console.ReadLine();
         }
     }
 }
