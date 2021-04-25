45d1a3aba79bdb1ff26f5da3319df3f598fde0e7
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-04-26 01:12:19 +0530

    Added file of UC4_DELETE_CONTACT

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace AddressBook
 {
+
     class AddressBook : IPerson
     {
         private Dictionary<string, Person> addressBook = new Dictionary<string, Person>(); //create addressBook Dictionary and string is (Key)(string=datatype) and person is (value)
@@ -39,6 +40,9 @@ namespace AddressBook
                 Console.WriteLine("Phone Number : " + item.Value.PhoneNumber + "\n");
 
             }
+
+
+
         }
         public void EditContact(string name) //method in inteface pass argument name
         {
@@ -87,12 +91,31 @@ namespace AddressBook
                     }
                 }
             }
+
         }
+
+        public void DeleteContact(string name)
+        {
+            if (addressBook.ContainsKey(name))
+            {
+                addressBook.Remove(name);
+                Console.WriteLine("\nDeleted Succesfully.\n");
+            }
+            else
+            {
+                Console.WriteLine("\nIt Is Not Found.\n");
+            }
+        }
+
+
+
     }
     interface IPerson
     {
         void AddContact(string firstName, string lastName, string address, string city, string state, string email, int zip, long phoneNumber);
         void EditContact(string name);
+        void DeleteContact(string name);
+        // void ViewContact();
     }
     public class Person
     {
@@ -114,30 +137,58 @@ namespace AddressBook
         {
             Console.WriteLine("-------------Welcome to Address Book Program------------ ");
             AddressBook addressBook = new AddressBook(); //object of AddessBook class
-            Console.WriteLine("Enter First Name :");
-            string firstName = Console.ReadLine();
-            Console.WriteLine("Enter Last Name :");
-            string lastName = Console.ReadLine();
-            Console.WriteLine("Enter Address :");
-            string address = Console.ReadLine();
-            Console.WriteLine("Enter City :");
-            string city = Console.ReadLine();
-            Console.WriteLine("Enter State :");
-            string state = Console.ReadLine();
-            Console.WriteLine("Enter Email :");
-            string email = Console.ReadLine();
-            Console.WriteLine("Enter Zip :");
-            int zip = Convert.ToInt32(Console.ReadLine());//conver into int using ToInt32()
-            Console.WriteLine("Enter Phone Number :");
-            long phoneNumber = Convert.ToInt64(Console.ReadLine());
-            addressBook.AddContact(firstName, lastName, address, city, state, email, zip, phoneNumber);
-            addressBook.ViewContact();//calling method
-            addressBook.EditContact(firstName);
-            addressBook.ViewContact();//calling method
-            Console.Read();
+
+            int Choice = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Enter First Name :");
+                string firstName = Console.ReadLine();
+                Console.WriteLine("Enter Last Name :");
+                string lastName = Console.ReadLine();
+                Console.WriteLine("Enter Address :");
+                string address = Console.ReadLine();
+                Console.WriteLine("Enter City :");
+                string city = Console.ReadLine();
+                Console.WriteLine("Enter State :");
+                string state = Console.ReadLine();
+                Console.WriteLine("Enter Email :");
+                string email = Console.ReadLine();
+                Console.WriteLine("Enter Zip :");
+                int zip = Convert.ToInt32(Console.ReadLine());//conver into int using ToInt32()
+                Console.WriteLine("Enter Phone Number :");
+                long phoneNumber = Convert.ToInt64(Console.ReadLine());
+                addressBook.AddContact(firstName, lastName, address, city, state, email, zip, phoneNumber);
+
+
+                switch (Choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter First Name Of Contact To Edit :");
+                        string nameToEdit = Console.ReadLine();
+                        addressBook.EditContact(nameToEdit);
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter First Name Of Contact To Delete :");
+                        string nameToDelete = Console.ReadLine();
+                        addressBook.DeleteContact(nameToDelete);
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter First Name Of Contact To View :");
+                        string nameToView = Console.ReadLine();
+                        addressBook.ViewContact();
+                        break;
+
+                    case 4:
+                        Console.WriteLine("It is Not Found Any Information.");
+                        break;
+                }
+            } while (Choice != 4);
         }
-
-
-
     }
 }
+
+
+
+
+
+
