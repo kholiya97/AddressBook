dfe4729c05bf15672d255a30cbebb74496d2a7c3
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-04-29 23:08:52 +0530

    Added file of UC6_REFACTOR_TO_ADD_MULTIPLE_ADDRESSBOOK

--- Program.cs
+++ Program.cs
@@ -9,59 +9,36 @@ namespace AddressBook
 
     class AddressBook : IPerson
     {
-        List<Person> adressBookList = new List<Person>(); //creating list
-        public void addPerson()
+        List<Person> adressBookList = new List<Person>();
+
+        public void AddContact(string firstName, string lastName, string address, string city, string state, string phoneNumber, string email)
         {
-            Console.WriteLine("Enter firstName:");//take input  user
-            string firstName = Console.ReadLine();//store input firstname variable
-
-            Console.WriteLine("Enter lastName:");//take input  user
-            string lastName = Console.ReadLine();//store input lastname variable
-
-            Console.WriteLine("Enter city:");//take input  user
-            string city = Console.ReadLine();//store input city variable
-
-            Console.WriteLine("Enter state:");//take input  user
-            string state = Console.ReadLine();//store input state variable
-
-            Console.WriteLine("Enter email:");//take input  user
-            string email = Console.ReadLine();//store input email variable
-
-            Console.WriteLine("Enter phoneNumber:");//take input  user
-            string phoneNumber = Console.ReadLine();//store input phonenumber variable
-
-
             Person person = new Person(firstName, lastName, city, state, email, phoneNumber);
-            adressBookList.Add(person);   //adding list data person
+            adressBookList.Add(person);
         }
-
-        public void displayPerson()//body of displayperson interface method
+        public void displayPerson()
         {
             Console.WriteLine("\nEntered Person Details is:");
             foreach (var person in adressBookList)
             {
-                Console.WriteLine("FirstName: {0}, LastName: {1}, city: {2}, state: {3}, email: {4}, phoneNumber: {5}", person.FirstName, person.LastName, person.City, person.State, person.Email, person.PhoneNumber);
+                Console.WriteLine("FirstName: {0}, LastName: {1}, city: {2}, state: {3}, email: {4}, phoneNumber: {5}", person.FirstName, person.LastName, person.city, person.state, person.email, person.phoneNumber);
             }
-
         }
-        public void editPerson()//body of editperson interface method
+        public void editPerson()
         {
-
-            Console.WriteLine("\n enter First name to edit details:");//take input
-            string name = Console.ReadLine();//store input name variable
+            Console.WriteLine("\n enter First name to edit details:");
+            string name = Console.ReadLine();
             foreach (var person in adressBookList)
             {
                 if (name.Equals(person.FirstName))
                 {
-
                     Console.WriteLine("Choose one of the following options: ");
                     Console.WriteLine("#1 Phone Number");
                     Console.WriteLine("#2 Email");
                     Console.WriteLine("#3 City");
                     Console.WriteLine("#4 State");
                     Console.WriteLine("#5 Quit");
-                    int choice = Convert.ToInt32(Console.ReadLine()); //converting into int with the help of ToInt32()
-
+                    int choice = Convert.ToInt32(Console.ReadLine());
                     switch (choice)
                     {
                         case 1:
@@ -93,28 +70,25 @@ namespace AddressBook
                             break;
                     }
                 }
-                else
-                    Console.WriteLine("Person is not registered");
             }
         }
 
-        public void deletePerson()//body of deleteperson interface method
+        public void deletePerson()
         {
-
             Console.WriteLine("Enter firstName of the user you want to remove");
-            string firstName = Console.ReadLine();
-
+            var firstName = Console.ReadLine();
             Console.WriteLine("Enter lastname of the user you want to remove");
-            string lastName = Console.ReadLine();
-
+            var lastName = Console.ReadLine();
             adressBookList.RemoveAll(item => item.FirstName == firstName && item.LastName == lastName);
-
-
         }
     }
-    interface IPerson
+
+    /// <summary>
+    /// signature of interface
+    /// </summary>
+    public interface IPerson
     {
-        void addPerson();
+        // void addPerson();
         void displayPerson();
         void editPerson();
         void deletePerson();
@@ -122,22 +96,23 @@ namespace AddressBook
 
     public class Person
     {
-
+        //instance variable
         public string FirstName;
         public string LastName;
-        public string City;
-        public string State;
-        public string Email;
-        public string PhoneNumber;
+        public string city;
+        public string state;
+        public string email;
+        public string phoneNumber;
+
         //parameterized constructor for initializing instance member
         public Person(string firstName, string lastName, string city, string state, string email, string phoneNumber)//Parameterized Constructor
         {
-            FirstName = firstName;
-            LastName = lastName;
-            City = city;
-            State = state;
-            Email = email;
-            PhoneNumber = phoneNumber;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.city = city;
+            this.state = state;
+            this.email = email;
+            this.phoneNumber = phoneNumber;
         }
         public String getFirstName() /// get method returns the value of the variable FirstName.
 		{
@@ -146,7 +121,7 @@ namespace AddressBook
 
         public void setFirstName(String firstName)  // set method assigns a value to the name variable.
         {
-            FirstName = firstName;
+            this.FirstName = firstName;
         }
 
         public String getLastName() //get method returns the value of the variable LastName.
@@ -156,93 +131,163 @@ namespace AddressBook
 
         public void setLastName(String lastName) //set method assigns a value to the name variable.
         {
-           LastName = lastName;
+            this.LastName = lastName;
         }
 
-        public String getCity()
+        public String getCity()//get method returns the value of the variable city.
         {
-            return City;
+            return city;
         }
 
-        public void setCity(String city)
+        public void setCity(String city)//get method returns the value of the variable city.
         {
-            City = city;
+            this.city = city;
         }
 
         public String getState()
         {
-            return State;
+            return state;
         }
 
         public void setState(String state)
         {
-            State = state;
+            this.state = state;
         }
         public String getPhoneNumber()
         {
-            return PhoneNumber;
+            return phoneNumber;
         }
 
         public void setPhoneNumber(String phoneNumber)
         {
-            PhoneNumber = phoneNumber;
+            this.phoneNumber = phoneNumber;
         }
 
         public String getEmail()
         {
-            return Email;
+            return email;
         }
 
         public void setEmail(String email)
         {
-            Email = email;
+            this.email = email;
+
         }
-    }
-    class Program
-    {
-
-
-        static void Main(string[] args)
+        class Program
         {
-            Console.WriteLine("-------------Welcome to Address Book Program------------ ");
-            AddressBook addressBook = new AddressBook(); //object of AddessBook class
-
-            bool ProgramIsRunning = true;
-            while (ProgramIsRunning)
+            /// <summary>
+            /// Entry ponit
+            /// </summary>
+            /// <param name="args"></param>
+            public static void Main(string[] args)
             {
-                Console.WriteLine("Choose one of the following options: ");
-                Console.WriteLine("#1 Add new user");
-                Console.WriteLine("#2 Display user information");
-                Console.WriteLine("#3 Edit user information");
-                Console.WriteLine("#4 Delete existing user");
-                Console.WriteLine("#5 Exit");
-                int num = Convert.ToInt32(Console.ReadLine());//convert into int with the help of ToInt32()
+                Console.WriteLine("Welcome in Address book System");
 
-                switch (num)//using switch case
+                ///create dictionary and 
+                ///Dict is name of dictionary
+                Dictionary<string, AddressBook> Dict = new Dictionary<string, AddressBook>();//string is Tkey and AddressBook is TValue.
+                bool ProgramIsRunning = true;
+
+                Console.WriteLine("\nHow many address Book you want to create : ");
+                int numAddressBooks = Convert.ToInt32(Console.ReadLine()); //store and convert into int using numAdressBooks variable.
+
+                for (int i = 1; i <= numAddressBooks; i++)
                 {
-                    case 1:
-                        addressBook.addPerson();
-                        break;
-                    case 2:
-                        addressBook.displayPerson();
-                        break;
-                    case 3:
-                        addressBook.editPerson();
-                        break;
-                    case 4:
-                        addressBook.deletePerson();
-                        break;
-                    case 5:
-                        ProgramIsRunning = false;
-                        break;
-                }//end switch case
+                    Console.WriteLine("Enter the name of address book " + i + ": ");
+                    string bookName = Console.ReadLine();
+                    AddressBook addressBook = new AddressBook(); //creating object of AddressBook class
+                    Dict.Add(bookName, addressBook); //add element in dictionary
+                }
+                Console.WriteLine("\nYou have created following Address Books : ");
+                foreach (var item in Dict) //var is used and it is store any data type value.
+                {
+                    Console.WriteLine("{0}", item.Key);
+                }
+                while (ProgramIsRunning)
+                {
+                    Console.WriteLine("\nChoose option to procced further \n1.Add Contact \n2.Edit Contact \n3.Delete Contact  \n4.Display Contacts \n5.Exit");
+                    int choice = Convert.ToInt32(Console.ReadLine()); ///store and convert into int using choice variable
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("\nEnter Existing Address Book Name for adding contacts");
+                            string contactName = Console.ReadLine();
+                            if (Dict.ContainsKey(contactName))
+                            {
+                                Console.WriteLine("\nEnter the number of contacts you want to add in address book");
+                                int numberOfContacts = Convert.ToInt32(Console.ReadLine());
+                                for (int i = 1; i <= numberOfContacts; i++)
+                                {
+                                    addContactBook(Dict[contactName]);
+                                }
+                                Dict[contactName].displayPerson();
+                            }
+                            else
+                            {
+                                Console.WriteLine("No AddressBook exist with name {0}", contactName);
+                            }
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter Address Book Name for edit contact");
+                            string editcontactName = Console.ReadLine();
+                            if (Dict.ContainsKey(editcontactName))
+                            {
+                                Dict[editcontactName].editPerson();
+                                Dict[editcontactName].displayPerson();
+                            }
+                            else
+                            {
+                                Console.WriteLine("No Address book exist with name {0} ", editcontactName);
+                            }
+                            break;
+                        case 3:
+                            Console.WriteLine("\nEnter Address Book Name for delete contact");
+                            string deleteContact = Console.ReadLine();
+                            if (Dict.ContainsKey(deleteContact))
+                            {
+                                Dict[deleteContact].deletePerson();
+                                Dict[deleteContact].displayPerson();
+                            }
+                            else
+                            {
+                                Console.WriteLine("No Address book exist with name {0} ", deleteContact);
+                            }
+                            break;
+                        case 4:
+                            Console.WriteLine("\nEnter Address Book Name for display contacts");
+                            string displayContactsInAddressBook = Console.ReadLine();
+                            Dict[displayContactsInAddressBook].displayPerson();
+                            break;
+                        case 5:
+                            ProgramIsRunning = false;
+                            break;
+                        default:
+                            Console.WriteLine("Please enter valid option");
+                            break;
+                    }
+                }
+
+                void addContactBook(AddressBook addressBook)
+                {
+                    Console.WriteLine("Enter First Name : ");
+                    string firstName = Console.ReadLine();
+                    Console.WriteLine("Enter Last Name : ");
+                    string lastName = Console.ReadLine();
+                    Console.WriteLine("Enter Address : ");
+                    string address = Console.ReadLine();
+                    Console.WriteLine("Enter City : ");
+                    string city = Console.ReadLine();
+                    Console.WriteLine("Enter State : ");
+                    string state = Console.ReadLine();
+                    Console.WriteLine("Enter Phone Number : ");
+                    string phoneNumber = Console.ReadLine();
+                    Console.WriteLine("Enter Email id :");
+                    string email = Console.ReadLine();
+                    addressBook.AddContact(firstName, lastName, address, city, state, phoneNumber, email);
+                }
+
             }
         }
     }
 }
-
-
-
-
-
 
