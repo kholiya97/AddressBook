71cca975333ee3152bf3f2efe3682016cfe7942a
kholiya97 <80846923+kholiya97@users.noreply.github.com>
2021-04-30 08:18:28 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -33,8 +33,7 @@ namespace Address_Book_System
         }
          static void Main(string[]args)
         {
-namespace Addres
-{
+
     class AddressBook : IPerson
     {
         List<Person> adressBookList = new List<Person>(); //creating list
