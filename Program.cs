9eddd4d9100c3d5fa841b7671e3de74980652a27
kholiya97 <80846923+kholiya97@users.noreply.github.com>
2021-05-03 07:55:22 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,51 +13,6 @@ namespace AddressBook
             AddPersonDetails personDetailsOBJ = new AddPersonDetails(); // created object of class  AddPersonDetails
             personDetailsOBJ.CreateMultipleUniqueAddressBook(); // call method CreateMultipleUniqueAddressBook with the help of object personDetailsOBJ
 
-
-            /*
-
-                        InterfaceDetails details = new AddPersonDetails(); // used concept of polymorphism 1 class having different forms
-                        bool check = true;
-                        while (check == true)
-                        {
-                            Console.WriteLine("=============================Welcome ToAddress Book Program==========================");
-                            Console.WriteLine("==========Please Enter Your Choice==========");
-                            Console.WriteLine("1. Add Details");
-                            Console.WriteLine("2. Display Details");
-                            Console.WriteLine("3. Edit Details");
-                            Console.WriteLine("4. Delete Details");
-                            Console.WriteLine("5. Exit");
-                            Console.WriteLine("=============================================");
-
-
-                            string choice = Console.ReadLine();
-                            int ch = Convert.ToInt32(choice);
-
-                            switch (ch)
-                            {
-                                case 1:
-                                    details.AddDetails();
-                                    break;
-                                case 2:
-                                    details.Display();
-                                    break;
-                                case 3:
-                                    Console.WriteLine("Please Enter First Name : ");
-                                    string name = Console.ReadLine();
-                                    details.Edit(name);
-                                    break;
-                                case 4:
-                                    Console.WriteLine("Please Enter First Name : ");
-                                    string nameForDeletion = Console.ReadLine();
-                                    details.Delete(nameForDeletion);
-                                    break;
-                                case 5:
-                                    return;
-                            }
-                        }
-
-                        */
-
         }
     }
 
